c999086fbb1fde24f3fc4cd06baa72ab4138bf64
octo <dlqhdgkr456@gmail.com>
2021-09-09 15:01:08 +0900

    2.2 useClick

---
+++ useClick.js
@@ -0,0 +1,20 @@
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    // check onClick is function
+    if (typeof onClick !== "function") {
+      return;
+    }
+    if (element.current) {
+      // for componentDidMount and componentDidUpdate
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      // for componentWillUnMount
+      if (element.current) {
+        element.current.removeEEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return typeof onClick !== "function" ? undefined : element;
+};
